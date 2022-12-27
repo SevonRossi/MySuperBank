152447b9478af2b210a1a478a951e171577b59df
Srossi28 <105078485+Srossi27@users.noreply.github.com>
2022-12-27 12:10:27 -0600

    Add project files.

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace MySuperBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Sevon", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+
+            account.MakeDeposit(-300, DateTime.Now, "stealing");
+            Console.Write(account.Balance);
+
+            BankAccount invalidAccount;
+
+
+            try
+            {
+                invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
